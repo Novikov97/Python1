2cd723eb66f11bb44e511ec7426b065e2bc45bf3
Vyacheslav <100835284+Novikov97@users.noreply.github.com>
2023-01-27 04:02:50 +0300

    Homework № 1

--- Homework №1.5.py
+++ Homework №1.5.py
@@ -2,6 +2,13 @@ receipt = int(input("Введите значение выручки фирмы "
 cost = int(input("Введите значение издержек фирмы "))
 
 if receipt > cost:
-    print("Прибыль - выручка больше издержек")
+    profit = receipt - cost
+    rent = profit / receipt
+    print(f"Финансовый результат - прибыль. Ее величина - {profit}. "
+          f"\nЗначит вычисляем рентабельность выручки (соотношение прибыли к выручке.)"
+          f"\nРентабельность выручки = {rent}.")
+    employee = int(input("Введите число сотрудников фирмы "))
+    profit_employee = profit / employee
+    print(f"Прибыль фирмы в расчете на одного сотрудника {profit_employee}.")
 else:
     print("Убыток - издержки больше выручки")
